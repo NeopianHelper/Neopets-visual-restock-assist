fc0c8cb83dc2d996e9910862bfe7ea3a1defcdee
NeopianHelper <43247662+NeopianHelper@users.noreply.github.com>
2023-10-05 21:41:45 -0400

    Update highlight.js
    comments

--- highlight.js
+++ highlight.js
@@ -1,46 +1,45 @@
-const currentURL = window.location.href;
+const currentURL = window.location.href; 
 console.log(currentURL);
 
-const urlParams = new URLSearchParams(new URL(currentURL).search);
+const urlParams = new URLSearchParams(new URL(currentURL).search);  //gets URL and the obj_type for purposes of finding what shop we are in
 const objType = urlParams.get('obj_type');
 
 function DetectItems(junkArray, goodArray) {
     let elementsLow = [];
     for (let value of junkArray) {
-        elementsLow.push(...document.querySelectorAll('[data-name="' + value + '"]'));
+        elementsLow.push(...document.querySelectorAll('[data-name="' + value + '"]'));  //compares junk item list array to elements in the shop
     }
     console.log(elementsLow);
     for (let elementLow of elementsLow) {
-        elementLow.style.opacity = '0.25';
+        elementLow.style.opacity = '0.25';    //applies opacity change
     }
 
     let elementsHigh = [];
     for (let value of goodArray) {
-        elementsHigh.push(...document.querySelectorAll('[data-name="' + value + '"]'));
+        elementsHigh.push(...document.querySelectorAll('[data-name="' + value + '"]')); //compares the highlight item list array to elements in the shop
     }
     console.log(elementsHigh);
     for (let elementHigh of elementsHigh) {
-        elementHigh.style.border = "5px solid #00FF40";
+        elementHigh.style.border = "5px solid #00FF40"; //applies a nice green border
     }
 }
 
 if (objType !== null) {
-    const ShopID = parseInt(objType, 10);
+    const ShopID = parseInt(objType, 10); //sets shopID to the objtype at the end of neo shop URLs
     
-    if (!isNaN(ShopID)) {
+    if (!isNaN(ShopID)) {                 //pulls from user data entered in option page based on ShopID
         console.log(ShopID);
         const junkKey = 'Junk' + ShopID;
         const goodKey = 'Good' + ShopID;
 
-        browser.storage.local.get([junkKey, goodKey]).then(data => {
+        browser.storage.local.get([junkKey, goodKey]).then(data => {     //transforms user provided item lists into arrays it can read
             const junkValue = data[junkKey] || "";
             const junkArray = junkValue.split("\n").map(item => item.trim());
 
             const goodValue = data[goodKey] || "";
             const goodArray = goodValue.split("\n").map(item => item.trim());
             
-            // Call the DOM manipulation function after fetching the data.
-            DetectItems(junkArray, goodArray);
+            DetectItems(junkArray, goodArray); //calls detectitems function to look at page and apply CSS
 
         }).catch(err => {
             console.error('Error retrieving data:', err);
