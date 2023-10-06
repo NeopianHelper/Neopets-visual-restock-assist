f277f10d91dcb575c1b26d77862749b37b91f340
NeopianHelper <43247662+NeopianHelper@users.noreply.github.com>
2023-10-05 21:45:10 -0400

    Update options.js
    comments

--- options.js
+++ options.js
@@ -3,42 +3,40 @@ document.getElementById('save').addEventListener('click', function() {
     let input1Value = document.getElementById('input1').value;
     let input2Value = document.getElementById('input2').value;
 
-    // Extract mode number from the string
+    
     let modeNum = mode.replace('mode', '');
 
-    // Construct the keys dynamically
-    let junkKey = 'Junk' + modeNum;
+  
+    let junkKey = 'Junk' + modeNum;        //reads and saves inputs dynamically since there are so many, based on shop IDs that will be called dynamically in the content script
     let goodKey = 'Good' + modeNum;
 
-    // Create an object to save
+    
     let saveData = {};
     saveData[junkKey] = input1Value;
     saveData[goodKey] = input2Value;
 
-    // Save data
+ 
     browser.storage.local.set(saveData);
 
-    // Optionally, give feedback to the user that data is saved.
     alert('Data saved');
 });
 
 function loadSavedData(mode) {
-    // Extract mode number from the string
+    // loading saved data is also dynamic
     let modeNum = mode.replace('mode', '');
 
-    // Construct the keys dynamically
     let junkKey = 'Junk' + modeNum;
     let goodKey = 'Good' + modeNum;
     let keys = [junkKey, goodKey];
 
     browser.storage.local.get(keys).then(results => {
-        document.getElementById('input1').value = results[junkKey] || '';  // Use an empty string as fallback
-        document.getElementById('input2').value = results[goodKey] || '';  // Use an empty string as fallback
+        document.getElementById('input1').value = results[junkKey] || '';  //empty string as fallback
+        document.getElementById('input2').value = results[goodKey] || '';  
     });
 }
 
-// Event listener for the dropdown change
-document.getElementById('dropdown').addEventListener('change', function() {
+
+document.getElementById('dropdown').addEventListener('change', function() { 
     let mode = this.value;
     loadSavedData(mode);
 });
